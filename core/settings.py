d957de17f52b036604d2d53eb6476e0b55ae6823
convxz <m.kadzaev77@gmail.com>
2026-03-13 19:24:52 +0300

    add dockerfile

--- core/settings.py
+++ core/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': 'catalog_db',
         'USER': 'catalog_user',
         'PASSWORD': 'winterarc1221',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
